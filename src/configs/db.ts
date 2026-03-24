6f1df428dd3a12a0555f3da72858223faeb6c1f2
vince dominic dela pena <vincedominic.delapena@gmail.com>
2026-03-25 07:33:54 +0800

    fix: ensure database connection uses specified port from environment variables

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -11,6 +11,7 @@ try {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: +(process.env.DB_PORT || 3306),
   });
 } catch (error) {
   throw Error;
